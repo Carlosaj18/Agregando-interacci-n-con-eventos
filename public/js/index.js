bc975e3a78cac318fdfbf753d1e1f466b7255f0a
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-04-19 13:20:24 -0500

    Se crean eventos de prueba en el proyecto

---
+++ public/js/index.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+    //JavaScript del Index
+    let container = document.querySelector('.container');
+    let subtitulo = document.querySelector('.subtitulo');
+    let destacado = document.querySelectorAll('p');
+    let fondo = document.querySelector('body');
+    let enlace = document.querySelector('a');
+    let logoDH = document.querySelector('.logoDH');
+    let menu = document.querySelector('#menu ul li');
+    
+    /*let nombre = prompt('Ingrese su nombre');
+    console.log(nombre);
+    if(nombre !=''){
+        subtitulo.innerHTML += nombre;
+    }else{
+        subtitulo.innerHTML += 'INVITADO';
+    }*/
+        
+    subtitulo.style.textTransform = 'uppercase';
+    let confirmar = confirm('Desea colocar un fondo de pantalla ')
+    if(confirmar){
+        fondo.classList.add('fondo');
+        enlace.style.color = '#E51B3E';
+    }
+    console.log(destacado);
+    for(let i = 0 ; i < destacado.length; i++){
+        if(i % 2 == 0){
+            destacado[i].classList.add('destacadoPar');
+        }else{
+            destacado[i].classList.add('destacadoImpar');
+        }
+    }
+    
+    /** 1. En index.ejs, agregar un evento para que cada vez que el usuario haga clic sobre el
+logo de Digital House se muestre el menú lateral con id="menu". REVISAR*/
+
+    logoDH.addEventListener('click', function(e){
+        console.log(e);
+        console.log(menu);
+        menu.style.display = 'block';
+        // menu.classList.toggle('#menu');
+    });
+
+    /** 2. En index.ejs, agregar un evento que permita ocultar el menú lateral cuando el
+mouse deje el área del menú. */
+
+    menu.addEventListener('mouseout', function(e){
+        console.log(e);
+        menu.style.display = 'none';
+    })
+
+}
---
+++ public/js/moviesAdd.js
@@ -0,0 +1,46 @@
+window.onload = function(){
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    let tituloInput = document.querySelector('#titulo')
+
+
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+    /** 4. En moviesAdd.ejs, establecer que, cada vez que se pase el mouse por el título
+'AGREGAR PELÍCULA', este cambie su color. */
+    titulo.addEventListener('mouseover', function(e){
+        titulo.style.color = 'orange'
+    })
+    titulo.addEventListener('mouseout', function(e) {
+        // modo oscuto
+        titulo.style.color = 'black'
+    })
+
+    let estadoSecreto = 0;
+
+    tituloInput.addEventListener('keypress', function(e){
+        if(estadoSecreto == 0 && e.key == "s"){ 
+            estadoSecreto =1;
+        } else if (estadoSecreto == 1 && e.key == "e"){
+            estadoSecreto =2;
+        } else if (estadoSecreto == 2 && e.key == "c"){
+            estadoSecreto =3;
+        } else if (estadoSecreto == 3 && e.key == "r"){
+            estadoSecreto =4;
+        } else if (estadoSecreto == 4 && e.key == "e"){
+            estadoSecreto =5;
+        } else if (estadoSecreto == 5 && e.key == "t"){
+            estadoSecreto =6;
+        } else if (estadoSecreto == 6 && e.key == "o"){
+            estadoSecreto =0;
+            alert('SECRETO MAGICO');
+        }  else {
+            estadoSecreto =0;
+        }
+    })
+
+}
